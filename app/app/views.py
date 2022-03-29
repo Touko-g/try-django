7a411521d484f787bed49e8a711fb48a2a531099
Molloy <2753219155@qq.com>
2022-03-29 22:44:03 +0800

    add

--- app/app/views.py
+++ app/app/views.py
@@ -1,5 +1,21 @@
+import random
+
 from django.http import HttpResponse
+from articles.models import Article
 
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    random_id = random.randint(1, 2)
+
+    article_obj = Article.objects.get(id=random_id)
+
+    h1_string = f"""
+        <h1>{article_obj.title}(id:{article_obj.id})</h1>
+    """
+    p_string = f"""
+        <p>{article_obj.content}</p>
+    """
+
+    html_string = h1_string + p_string
+
+    return HttpResponse(html_string)
